81b469f7121e793c47df93c4f6c519c270c4e86d
xiezhi <244761537@qq.com>
2018-12-11 14:50:19 +0800

    修改了视图显示数据

--- src/main/java/com/xt/controller/DeptController.java
+++ src/main/java/com/xt/controller/DeptController.java
@@ -3,26 +3,40 @@ package com.xt.controller;
 import com.xt.pojo.Dept;
 import com.xt.service.DeptServiceI;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * controller控制器
+ * @author  xz
+ */
 @Controller
 public class DeptController {
 
     @Resource
     private DeptServiceI deptService;
 
+    /**
+     * 请求index 跳转到 getAll的url
+     * @return
+     */
     @RequestMapping("/index")
     public String index(){
-        System.out.print("hello index");
         return "redirect:getAll.action";
     }
 
+    /**
+     * 获取所有的部门信息 并返回到页面 打印数据
+     * @param model
+     * @return
+     */
     @RequestMapping("/getAll")
-    public String getAll(){
+    public String getAll(Model model){
         List<Dept> list = deptService.getAll();
         System.out.print(list);
+        model.addAttribute("ll",list);
         return "all";
     }
 }
--- src/main/webapp/WEB-INF/jsp/all.jsp
+++ src/main/webapp/WEB-INF/jsp/all.jsp
@@ -5,12 +5,18 @@
   Time: 11:14
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java"
+isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-    this is all jsp
+    this is all jsp<br/>
+    <c:forEach items="${ll}" var="dept">
+        编号:${dept.deptNo}--部门:${dept.dName}--位置:${dept.loc}<br/>
+    </c:forEach>
+
 </body>
 </html>
